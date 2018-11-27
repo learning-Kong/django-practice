6b20497dd95f99069bfc425076cd97b15f68656c
kongxianglei <1032805171@qq.com>
2018-11-27 10:46:50 +0800

    01

--- S20_project/app01/views.py
+++ S20_project/app01/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def info(request):
+    pass
+    return HttpResponse('hello world')
